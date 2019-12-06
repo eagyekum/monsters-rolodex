e0a5a58f3c1b01f22b362ad0414f4dc7f9241583
DESKTOP-PQAUVA8\Ernest <ernest.agyekum@minex360.com>
2019-12-06 01:27:27 +0000

    final production build v1

---
+++ precache-manifest.4c5d141022ee596a1d202d1265a33c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "016dcd50529dbcc91bf5f0ccc21db730",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6375166a10a66e113a6a",
+    "url": "/static/css/main.4c9d9ceb.chunk.css"
+  },
+  {
+    "revision": "d706f28003964d78c4b2",
+    "url": "/static/js/2.1339efe9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1339efe9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6375166a10a66e113a6a",
+    "url": "/static/js/main.d925f296.chunk.js"
+  },
+  {
+    "revision": "58f9d56a7c0878449fd5",
+    "url": "/static/js/runtime-main.0e4976a9.js"
+  }
+]);
